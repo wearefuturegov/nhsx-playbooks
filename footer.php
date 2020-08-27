5db440576252f44e30c1fe9502ab227f15ca8335
Jaye Hackett <jayehackett@FutureGovs-MacBook-Pro.local>
2020-08-27 11:22:48 +0100

    update footer links

--- footer.php
+++ footer.php
@@ -3,10 +3,10 @@
         <div class="nhsuk-width-container">
             <h2 class="nhsuk-u-visually-hidden">Support links</h2>
             <ul class="nhsuk-footer__list nhsuk-footer__list">
-                <li class="nhsuk-footer__list-item"><a class="nhsuk-footer__list-item-link" href="/accessibility-statement/">Accessibility statement</a></li>
-                <li class="nhsuk-footer__list-item"><a class="nhsuk-footer__list-item-link" href="/cookies/">Cookies</a></li>
-                <li class="nhsuk-footer__list-item"><a class="nhsuk-footer__list-item-link" href="/privacy-policy/">Privacy policy</a></li>
-                <li class="nhsuk-footer__list-item"><a class="nhsuk-footer__list-item-link" href="/terms-and-conditions/">Terms and conditions</a></li>
+                <li class="nhsuk-footer__list-item"><a class="nhsuk-footer__list-item-link" href="https://nhsx.nhs.uk/accessibility-statement/">Accessibility statement</a></li>
+                <li class="nhsuk-footer__list-item"><a class="nhsuk-footer__list-item-link" href="https://nhsx.nhs.uk/cookies/">Cookies</a></li>
+                <li class="nhsuk-footer__list-item"><a class="nhsuk-footer__list-item-link" href="https://nhsx.nhs.uk/privacy-policy/">Privacy policy</a></li>
+                <li class="nhsuk-footer__list-item"><a class="nhsuk-footer__list-item-link" href="https://nhsx.nhs.uk/terms-and-conditions/">Terms and conditions</a></li>
             </ul>
             <p class="nhsuk-footer__copyright">&copy; Crown copyright</p>
         </div>
